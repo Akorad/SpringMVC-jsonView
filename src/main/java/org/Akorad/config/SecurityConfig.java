335354318199c3a94a78a262b8d66e00c23c7724
Akorad <vlad179711@gmail.com>
2025-09-22 14:42:05 +0400

    Spring Security - простая конфигурация

---
+++ src/main/java/org/Akorad/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package org.Akorad.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/home").permitAll() // Public endpoints
+                    .anyRequest().authenticated() // All other endpoints require authentication
+                )
+                .formLogin(from -> from
+                    .loginPage("/login") // Custom login page
+                    .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // In-memory user details manager with a single user for demonstration
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
